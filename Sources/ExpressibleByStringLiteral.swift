5ba7f780361bb0596291db0ca63a7509579b8420
therealbnut <andrew.charles.bennett@gmail.com>
2017-01-28 11:56:13 +1100

    Inline Documentation

--- Sources/ExpressibleByStringLiteral.swift
+++ Sources/ExpressibleByStringLiteral.swift
@@ -1,9 +1,18 @@
 
+// MARK: - ExpressibleByStringLiteral
+
+/// When implementing ExpressibleByStringLiteral
+///  * No need to implement init(extendedGraphemeClusterLiteral:)
+///  * No need to implement init(unicodeScalarLiteral:)
+
 extension ExpressibleByExtendedGraphemeClusterLiteral where Self: ExpressibleByStringLiteral {
     public init(extendedGraphemeClusterLiteral value: StringLiteralType) {
         self.init(stringLiteral: value)
     }
 }
+
+/// When implementing ExpressibleByExtendedGraphemeClusterLiteral
+///  * No need to implement init(unicodeScalarLiteral:)
 
 extension ExpressibleByUnicodeScalarLiteral where Self: ExpressibleByExtendedGraphemeClusterLiteral {
     public init(unicodeScalarLiteral value: ExtendedGraphemeClusterLiteralType) {
